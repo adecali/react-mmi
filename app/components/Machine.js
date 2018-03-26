d4b3b87f2c8e5610eab82e8b2d78964a2c1ac4f6
Laura BP <laura.brun.picard@gmail.com>
2018-03-26 06:53:26 +0000

    Création du composant Machine

--- app/components/Machine.js
+++ app/components/Machine.js
@@ -8,7 +8,21 @@ import React from 'react';
 class Machine extends React.Component {
   render() {
     // Dans tous les cas, afficher
-    return <p>Machine 1</p>
+    return (
+      <div className="Machine">
+          <div className="MachineHeader">
+            <h3>Machine pour le {this.props.tittle}</h3>
+          </div>
+          
+            <p className="Machine1">
+             {this.props.tittle} | {this.props.isActive}
+            </p>
+            
+                <div className="MachineFooter">
+                <h4>{this.props.tittle}</h4>
+              </div>
+      </div>
+      );
   }
 }
 
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,13 +7,23 @@ import React from 'react';
 
 // On importe ici les composants qu'on veut afficher
 import Machine from '../../components/Machine.js';
+import Header from '../../components/Header.js';
+import Footer from '../../components/Footer.js';
+
 // On peut aussi importer du CSS de la meme facon.
+import css from '../../css/styles.css';
 
 export default function App() {
   return (
     // Un return doit retourner un seul élément du DOM
     // Si on veut afficher plusieurs éléments adjacents,
     // On devra donc les encapsuler dans une DIV parente.
-    <Machine />
-  );
+   <div>
+       <Header/>
+
+      <Machine tittle="thé" isActive="yes" />
+      <Machine tittle="chocolat"isActive="no" /> 
+      <Machine tittle="café"isActive="no" />
+      <Footer/>
+   </div> );
 }
